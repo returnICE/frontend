76a9f6c160b38be40acac422d0ade3b3a4ffc21c
seongwonJeong <rozencrants@naver.com>
2020-05-04 19:01:03 +0900

    dev#Ja05-식당 회원가입 구현

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,18 +2,45 @@
   <div class="login">
     <div id = "login_bg">
       <img src = "../assets/login_page/logo.png" id = "logo"/>
-      <div id = "login_container" align = "left">
-        <div class = "id_container">
-        ID <input id = "textbox_id" v-model = "ID" placeholder="ID" autocomplete="on"/>
+      <div id = "login_container">
+        <div class = "input_row">
+        ID
+        <input type = "text" v-model="user.id" id = "textbox_id" placeholder="ID" autocomplete="on"/>
         </div>
-        <div class = "pw_container">
-        PW <input id = "textbox_pw" v-model = "PW" placeholder="PW"/>
+        <div class = "input_row">
+        PW
+        <input type = "password" v-model="user.pw" id = "textbox_pw" placeholder="PW"/>
         </div>
-        <img src = "../assets/login_page/btn_login.png" id = "btn_login"/>
-        <img src = "../assets/login_page/btn_signup.png" id = "btn_signup"/>
+        <button v-on:click = "login" id = "btn_login"/>
+        <button v-on:click = "signUp" id="btn_signup"></button>
       </div>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data: function () {
+    return {
+      user: {
+        id: '',
+        pw: ''
+      }
+    }
+  },
+  methods: {
+    signUp: function () {
+      window.location.href = '/signUp'
+    },
+    login: function () {
+      this.$http.post('/api/users/login', {
+        user: this.user
+      }).then(
+        (res) => {
+          alert(res.data.message)
+        })
+    }
+  }
+}
+</script>
 <style src = "./login.css" scoped>
 </style>
---
+++ src/views/signup.vue
@@ -0,0 +1,111 @@
+<template>
+<div id = 'signup_page'>
+  <div id = 'signup_container'>
+    <div class = "input_container">
+    <br/>
+    <h1>회원가입</h1>
+        ID : <input type="text" v-model="user.id" id="signup_id" class = "text_box"/><br/>
+        PW : <input type="password" v-model="user.pw" id="signup_pw" class = "text_box"/><br/>
+        PW 확인: <input type="password" v-model="pwcollect" id="signup_pw_accept" class = "text_box"/><br/>
+        <div v-if = "user.pw != pwcollect && pwcollect != ''" class = "password_collect">비밀번호가 다릅니다</div>
+        매장 이름: <input type="text" v-model="user.name" id="signup_name" class = "text_box"/><br/>
+        매장 전화번호 :
+        <input type = "text" v-model="firstphone" id="signup_phone_first" class = "input_phone"/>-
+        <input type = "text" v-model="secondphone" id="signup_phone_second" class = "input_phone"/>-
+        <input type = "text" v-model="thirdphone" id="signup_phone_third" class = "input_phone"/><br/>
+        매장 소개 : <input type = "text_box" v-model="user.info" id = "signup_info" class = "text_box"/><br/><br/>
+        매장 사진 : <br/><br/><input type = "file" id = "singup_image" @change="onFileChange"/><br/><br/>
+    <form>
+        기업 계약 여부 :
+        <input type="radio" v-model="user.contractable" id="signup_contractable" name="contractable" value="true"/>
+        <label value="true" for = "true"> 허용 </label>
+        <input type="radio" v-model="user.contractable" id="signup_contractable" name="contractable" value="false"/>
+        <label value="false" for = "false"> 거부 </label><br/><br/>
+    </form>
+    주소 : <br/>
+    <input type = "text" id = "signup_address_num" class = "textbox_address" placeholder="우편번호" disabled/>
+    <button v-on:click = "address_function">주소찾기</button><br/><br/>
+    <input v-model="user.address" type = "text" id = "signup_address" class = "textbox_address" placeholder="주소" disabled/><br/><br/>
+    매장 종류 :
+    <select v-model="user.type">
+      <option disabled value="">선택</option>
+      <option value="bar">술집</option>
+      <option value="dinner">음식점</option>
+      <option value="cafe">카페</option>
+    </select>
+    <br/><br/>
+    <button v-on:click = "signup" id = "btn_signup"/>
+    </div>
+    </div>
+</div>
+</template>
+<script src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"></script>
+<script>
+export default {
+  data: function(){
+    return{
+      firstphone : '',
+      secondphone: '',
+      thirdphone: '',
+      pwcollect:'',
+      user: {
+        id: '',
+        pw: '',
+        name:'',
+        phone: '',
+        address:'',
+        lat:'',
+        lon:'',
+        info:'',
+        imgURL:'',
+        contractable:'',
+        type:''
+      }
+    }
+  },
+  methods: {
+    address_function: function () {
+      new daum.Postcode ({
+        oncomplete: function (data) {
+          document.getElementById('signup_address').value = data.address
+          document.getElementById('signup_address_num').value = data.zonecode
+          var geocoder = new google.maps.Geocoder();
+          var addr=data.address;
+          var lat="";
+          var lng="";
+          geocoder.geocode({'address':addr},
+            function(results, status){
+              if(results!=""){
+                var location=results[0].geometry.location;
+                lat=location.lat();
+                lng=location.lng();
+              }
+            }
+          )
+        }
+      }).open()
+    },
+    signup : function(event){
+      user.phone = firstphone + secondphone + thirdphone
+        this.$http.post('users/signUp',{
+          user : this.user
+        }).then((res) => {
+          if(res.data.success == true){
+            alert(res.data.message)
+            this.$router.push('/login')
+          }
+          if(res.data.success == false){
+            alert(res.data.message)
+          }
+        }).catch(function(error){
+          alert(error)
+        })
+    },
+    onFileChange : function(){
+
+    }
+  }
+}
+</script>
+<style src = "./signup.css" scoped>
+</style>
